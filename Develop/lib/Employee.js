4c1fbdc3361af81722b126b00612f8f665f3be31
dejesusf <dejesusf@uw.edu>
2023-01-26 01:31:44 -0800

    code passed tests for Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
-// TODO: Write code to define and export the Employee class
+// Write code to define and export the Employee class
+const inquirer= require('inquirer');
+
+class Employee {
+    constructor(name, id, email) {
+        this.name= name;
+        this.id= id;
+        this.email= email;
+    }
+    getName() {
+        console.log(this.name);
+        return this.name;
+    }
+    getId() {
+        console.log(this.id);
+        return this.id;
+    }
+    getEmail(){
+        console.log(this.email);
+        return this.email;
+    }
+    getRole(){
+        console.log('Employee');
+        return 'Employee';
+    }
+}
+
+module.exports= Employee;
